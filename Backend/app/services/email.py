50e213ec015c63476d1cb68744725b3568c9efff
Benisco007 <etondji79@gmail.com>
2026-07-27 21:55:50 +0100

    Fix email port 465 SSL

--- Backend/app/services/email.py
+++ Backend/app/services/email.py
@@ -5,10 +5,10 @@ conf = ConnectionConfig(
     MAIL_USERNAME=settings.MAIL_USERNAME,
     MAIL_PASSWORD=settings.MAIL_PASSWORD,
     MAIL_FROM=settings.MAIL_FROM,
-    MAIL_PORT=587,
-    MAIL_SERVER=settings.MAIL_SERVER,
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
+    MAIL_PORT=465,
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=False
 )
